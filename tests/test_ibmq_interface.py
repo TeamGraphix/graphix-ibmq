65c42e4d26e048d3aeb9a1d8962ad0ed72a92506
d1ssk <daichi.sasaki.00@gmail.com>
2023-04-26 20:17:28 +0900

    seperate random_circuit.py in tests

--- tests/test_ibmq_interface.py
+++ tests/test_ibmq_interface.py
@@ -1,8 +1,8 @@
 import unittest
 import numpy as np
-from graphix import Circuit
 from qiskit import Aer, transpile
 from graphix_ibmq.runner import IBMQBackend
+import tests.random_circuit as rc
 
 def modify_statevector(statevector, output_qubit):
     N = round(np.log2(len(statevector)))
@@ -15,52 +15,12 @@ def modify_statevector(statevector, output_qubit):
         new_statevector[int(new_idx, 2)] += statevector[i]
     return new_statevector
 
-# copy of graphix/tests/random_cuircuit.py
-def first_rotation(circuit, nqubits):
-    for qubit in range(nqubits):
-        circuit.rx(qubit, np.random.rand())
-
-
-def mid_rotation(circuit, nqubits):
-    for qubit in range(nqubits):
-        circuit.rx(qubit, np.random.rand())
-        circuit.rz(qubit, np.random.rand())
-
-
-def last_rotation(circuit, nqubits):
-    for qubit in range(nqubits):
-        circuit.rz(qubit, np.random.rand())
-
-
-def entangler(circuit, pairs):
-    for a, b in pairs:
-        circuit.cnot(a, b)
-
-
-def entangler_rzz(circuit, pairs):
-    for a, b in pairs:
-        circuit.rzz(a, b, np.random.rand())
-
-def generate_gate(nqubits, depth, pairs, use_rzz=False):
-    circuit = Circuit(nqubits)
-    first_rotation(circuit, nqubits)
-    entangler(circuit, pairs)
-    for k in range(depth - 1):
-        mid_rotation(circuit, nqubits)
-        if use_rzz:
-            entangler_rzz(circuit, pairs)
-        else:
-            entangler(circuit, pairs)
-    last_rotation(circuit, nqubits)
-    return circuit
-
-
 class TestIBMQInterface(unittest.TestCase):
     def test_to_qiskit(self):
         nqubits = 5
         depth = 5
         pairs = [(i, np.mod(i + 1, nqubits)) for i in range(nqubits)]
-        circuit = generate_gate(nqubits, depth, pairs)
+        circuit = rc.generate_gate(nqubits, depth, pairs)
         pattern = circuit.transpile()
         state = pattern.simulate_pattern()
         
