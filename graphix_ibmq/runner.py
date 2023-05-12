743a8db837aa4159d1eca0548b25b610f63f0701
d1ssk <daichi.sasaki.00@gmail.com>
2023-05-12 19:33:37 +0900

    reformat using black

--- graphix_ibmq/runner.py
+++ graphix_ibmq/runner.py
@@ -5,6 +5,7 @@ from qiskit import transpile
 from qiskit.providers.ibmq import least_busy
 from graphix.clifford import CLIFFORD_CONJ
 
+
 class IBMQBackend:
     """runs MBQC pattern with IBM quantum device."""
 
@@ -32,8 +33,14 @@ class IBMQBackend:
             self.resource = resource
             self.backend = self.provider.get_backend(self.resource)
         else:
-            self.backend = least_busy(self.provider.backends(filters=lambda b: b.configuration().n_qubits >= 1 and not b.configuration().simulator and b.status().operational==True))
-            self.resource = self.backend.name       
+            self.backend = least_busy(
+                self.provider.backends(
+                    filters=lambda b: b.configuration().n_qubits >= 1
+                    and not b.configuration().simulator
+                    and b.status().operational == True
+                )
+            )
+            self.resource = self.backend.name
         print(f"Using backend {self.backend.name}")
 
     def to_qiskit(self, save_statevector=False):
@@ -55,80 +62,80 @@ class IBMQBackend:
         empty_qubit = [i for i in range(n)]  # list indicating the free circuit qubits
         qubit_dict = {}  # dictionary to record the correspondance of pattern nodes and circuit qubits
 
-        for cmd in self.pattern.seq: 
+        for cmd in self.pattern.seq:
 
-            if cmd[0] == 'N':
+            if cmd[0] == "N":
                 circ_ind = empty_qubit[0]
                 empty_qubit.pop(0)
                 circ.reset(circ_ind)
                 circ.h(circ_ind)
                 qubit_dict[cmd[1]] = circ_ind
 
-            if cmd[0] == 'E':
+            if cmd[0] == "E":
                 circ.cz(qubit_dict[cmd[1][0]], qubit_dict[cmd[1][1]])
 
-            if cmd[0] == 'M':
+            if cmd[0] == "M":
                 circ_ind = qubit_dict[cmd[1]]
                 plane = cmd[2]
-                alpha = cmd[3]*np.pi
+                alpha = cmd[3] * np.pi
                 s_list = cmd[4]
                 t_list = cmd[5]
 
                 if len(cmd) == 6:
-                    if plane == 'XY':
-                        # act p and h to implement non-Z-basis measurement 
+                    if plane == "XY":
+                        # act p and h to implement non-Z-basis measurement
                         if alpha != 0:
-                            for s in s_list: # act x every time 1 comes in the s_list
+                            for s in s_list:  # act x every time 1 comes in the s_list
                                 with circ.if_test((cr[s], 1)):
                                     circ.x(circ_ind)
-                            circ.p(-alpha, circ_ind) # align |+_alpha> (or |+_-alpha>) with |+>
+                            circ.p(-alpha, circ_ind)  # align |+_alpha> (or |+_-alpha>) with |+>
 
-                        for t in t_list: # act z every time 1 comes in the t_list
+                        for t in t_list:  # act z every time 1 comes in the t_list
                             with circ.if_test((cr[t], 1)):
                                 circ.z(circ_ind)
-                
-                        circ.h(circ_ind) # align |+> with |0>
-                        
-                        circ.measure(circ_ind, cmd[1]) # measure and store the result
-                        empty_qubit.append(circ_ind) # liberate the circuit qubit
+
+                        circ.h(circ_ind)  # align |+> with |0>
+
+                        circ.measure(circ_ind, cmd[1])  # measure and store the result
+                        empty_qubit.append(circ_ind)  # liberate the circuit qubit
 
                 elif len(cmd) == 7:
                     cid = cmd[6]
                     for op in CLIFFORD_TO_QISKIT[CLIFFORD_CONJ[cid]]:
                         exec(f"circ.{op}({circ_ind})")
 
-                    if plane == 'XY':
-                        # act p and h to implement non-Z-basis measurement 
+                    if plane == "XY":
+                        # act p and h to implement non-Z-basis measurement
                         if alpha != 0:
-                            for s in s_list: # act x every time 1 comes in the s_list
+                            for s in s_list:  # act x every time 1 comes in the s_list
                                 with circ.if_test((cr[s], 1)):
                                     circ.x(circ_ind)
-                            circ.p(-alpha, circ_ind) # align |+_alpha> (or |+_-alpha>) with |+>
+                            circ.p(-alpha, circ_ind)  # align |+_alpha> (or |+_-alpha>) with |+>
 
-                        for t in t_list: # act z every time 1 comes in the t_list
+                        for t in t_list:  # act z every time 1 comes in the t_list
                             with circ.if_test((cr[t], 1)):
                                 circ.z(circ_ind)
-                
-                        circ.h(circ_ind) # align |+> with |0>
-                        
-                        circ.measure(circ_ind, cmd[1]) # measure and store the result
-                        empty_qubit.append(circ_ind) # liberate the circuit qubit
 
-            if cmd[0] == 'X':
+                        circ.h(circ_ind)  # align |+> with |0>
+
+                        circ.measure(circ_ind, cmd[1])  # measure and store the result
+                        empty_qubit.append(circ_ind)  # liberate the circuit qubit
+
+            if cmd[0] == "X":
                 circ_ind = qubit_dict[cmd[1]]
                 s_list = cmd[2]
                 for s in s_list:
                     with circ.if_test((cr[s], 1)):
                         circ.x(circ_ind)
 
-            if cmd[0] == 'Z':
+            if cmd[0] == "Z":
                 circ_ind = qubit_dict[cmd[1]]
                 s_list = cmd[2]
                 for s in s_list:
                     with circ.if_test((cr[s], 1)):
                         circ.z(circ_ind)
 
-            if cmd[0]  == 'C':
+            if cmd[0] == "C":
                 circ_ind = qubit_dict[cmd[1]]
                 cid = cmd[2]
                 for op in CLIFFORD_TO_QISKIT[cid]:
@@ -151,8 +158,8 @@ class IBMQBackend:
                 circ.measure(circ_ind, node)
 
             self.circ = circ
-        
-    def transpile(self, optimization_level = 1):
+
+    def transpile(self, optimization_level=1):
         """transpile the circuit for the designated resource.
         Parameteres
         -----------
@@ -160,7 +167,7 @@ class IBMQBackend:
             the optimization level of the transpilation.
         """
         self.circ = transpile(self.circ, backend=self.backend, optimization_level=optimization_level)
-    
+
 
 CLIFFORD_TO_QISKIT = [
     ["id"],
--- tests/test_ibmq_interface.py
+++ tests/test_ibmq_interface.py
@@ -4,16 +4,18 @@ from qiskit import Aer, transpile
 from graphix_ibmq.runner import IBMQBackend
 import tests.random_circuit as rc
 
+
 def modify_statevector(statevector, output_qubit):
     N = round(np.log2(len(statevector)))
-    new_statevector = np.zeros(2**len(output_qubit), dtype=complex)
+    new_statevector = np.zeros(2 ** len(output_qubit), dtype=complex)
     for i in range(len(statevector)):
-        i_str = format(i, f'0{N}b')
+        i_str = format(i, f"0{N}b")
         new_idx = ""
         for idx in output_qubit:
             new_idx += i_str[N - idx - 1]
         new_statevector[int(new_idx, 2)] += statevector[i]
     return new_statevector
+
 
 class TestIBMQInterface(unittest.TestCase):
     def test_to_qiskit(self):
@@ -23,11 +25,11 @@ class TestIBMQInterface(unittest.TestCase):
         circuit = rc.generate_gate(nqubits, depth, pairs)
         pattern = circuit.transpile()
         state = pattern.simulate_pattern()
-        
+
         ibmq_backend = IBMQBackend(pattern)
-        simulator = Aer.get_backend('aer_simulator')
+        simulator = Aer.get_backend("aer_simulator")
         ibmq_backend.to_qiskit(save_statevector=True)
-        qiskit_circuit = transpile(ibmq_backend.circ, simulator) 
+        qiskit_circuit = transpile(ibmq_backend.circ, simulator)
         sim_result = simulator.run(qiskit_circuit).result()
         state_qiskit = sim_result.get_statevector(qiskit_circuit)
         state_qiskit_mod = modify_statevector(state_qiskit, ibmq_backend.circ_output)
@@ -35,5 +37,5 @@ class TestIBMQInterface(unittest.TestCase):
         np.testing.assert_almost_equal(np.abs(np.dot(state_qiskit_mod.conjugate(), state.flatten())), 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
