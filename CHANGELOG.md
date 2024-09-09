f229e1c6e857a199908a2e336047ac4904a62386
Daichi Sasaki <daichi.sasaki.00@gmail.com>
2024-09-09 17:57:53 +0900

    Update for compatibility with Qiskit 1.0 (#15)
    * update for compatibility with qiskit 1.0
    
    * black
    
    * modify tests
    
    * modify sim method and tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 ### Changed
+- Updated to support Qiskit 1.0.
 
 ### Fixed
 
--- tests/test_ibmq_interface.py
+++ tests/test_ibmq_interface.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-import tests.random_circuit as rc
+import random_circuit as rc
 from graphix_ibmq.runner import IBMQBackend
 
 
@@ -24,7 +24,7 @@ class TestIBMQInterface(unittest.TestCase):
         depth = 5
         pairs = [(i, np.mod(i + 1, nqubits)) for i in range(nqubits)]
         circuit = rc.generate_gate(nqubits, depth, pairs)
-        pattern = circuit.transpile()
+        pattern = circuit.transpile().pattern
         state = pattern.simulate_pattern()
 
         ibmq_backend = IBMQBackend(pattern)
@@ -40,7 +40,7 @@ class TestIBMQInterface(unittest.TestCase):
         depth = 5
         pairs = [(i, np.mod(i + 1, nqubits)) for i in range(nqubits)]
         circuit = rc.generate_gate(nqubits, depth, pairs)
-        pattern = circuit.transpile()
+        pattern = circuit.transpile().pattern
         pattern.perform_pauli_measurements()
         pattern.minimize_space()
         state = pattern.simulate_pattern()
